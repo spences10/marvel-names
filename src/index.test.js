36e1fc88c498fc09181a993716b0b6c04e4d9575
spences10 <spences10apps@gmail.com>
2017-04-26 11:45:47 +0100

    write tests

--- src/index.test.js
+++ src/index.test.js
@@ -1,9 +1,27 @@
-var expect = require('chai').expect
-var marvel = require('./index')
+var expect = require('chai').expect;
+var marvel = require('./index');
 
-describe('marvel-names', function () {
-  it('should work', function () {
-    expect(true).to.be.true
+describe('marvel-names', function() {
+  describe('all', function() {
+    it('should be an array of strings', function() {
+      expect(marvel.all).to.satisfy(isArrayOfStrings)
+
+      function isArrayOfStrings(array) {
+        return array.every(function(item) {
+          return typeof item === 'string'
+        })
+      }
+    })
+
+    it('should contain `Wolverine`', function() {
+      expect(marvel.all).to.include('Wolverine')
+    })
+  })
+
+  describe('random', function() {
+    it('should return a random item from the marvel.all', function() {
+      var randomItem = marvel.random()
+      expect(marvel.all).to.include(randomItem)
+    })
   })
 })
-
