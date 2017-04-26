055e696cb27fcfe79276edfa6c59b741489156ea
spences10 <spences10apps@gmail.com>
2017-04-26 11:32:12 +0100

    set up mocha and chai

---
+++ src/index.test.js
@@ -0,0 +1,9 @@
+var expect = require('chai').expect
+var marvel = require('./index')
+
+describe('marvel-names', function () {
+  it('should work', function () {
+    expect(true).to.be.true
+  })
+})
+
